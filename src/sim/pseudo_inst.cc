978e3f4d1b05df9c96dc75afe11858b8435494b5
Ali Asadi <aliasadi@cafebazaar.ir>
2019-01-28 20:28:09 +0330

    Add timeout for instruction
    Change-Id: Ib29928c5660e697073df3e798871569d8049d47c

--- src/sim/pseudo_inst.cc
+++ src/sim/pseudo_inst.cc
@@ -595,7 +595,7 @@ switchcpu(ThreadContext *tc)
 int64_t
 crossop(ThreadContext *tc, int64_t x1, int64_t y1, int64_t x2, int64_t y2)
 {
-    this_thread::sleep_for(chrono::nanoseconds(100000));
+    this_thread::sleep_for(chrono::nanoseconds(32 * 20));
     return x1 * y2 - x2 * y1;
 }
 
